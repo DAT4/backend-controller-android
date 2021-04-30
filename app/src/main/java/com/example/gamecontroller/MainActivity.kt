684dbb08124dea3df2c233d9730cb7ca12d339b5
martin <mail@mama.sh>
2021-04-30 23:11:39 +0200

    Buttons will vibrate

--- app/src/main/java/com/example/gamecontroller/MainActivity.kt
+++ app/src/main/java/com/example/gamecontroller/MainActivity.kt
@@ -1,13 +1,22 @@
 package com.example.gamecontroller
 
+import android.content.Context
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.VibrationEffect
+import android.os.Vibrator
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import com.example.gamecontroller.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(), controller.AndroidCallback {
 
     private lateinit var binding: ActivityMainBinding
+    private val vibrator by lazy {
+        getSystemService(
+        Context.VIBRATOR_SERVICE) as Vibrator
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,17 +27,27 @@ class MainActivity : AppCompatActivity(), controller.AndroidCallback {
         setContentView(binding.root)
     }
 
+    private fun vibrate(){
+        vibrator.vibrate(
+            VibrationEffect.createOneShot(
+                50, VibrationEffect.DEFAULT_AMPLITUDE))
+    }
+
     private fun registerButtons() {
         binding.btnDown.setOnClickListener {
+            vibrate()
             controller.Controller.move(controller.Controller.DOWN)
         }
         binding.btnUp.setOnClickListener {
+            vibrate()
             controller.Controller.move(controller.Controller.UP)
         }
         binding.btnLeft.setOnClickListener {
+            vibrate()
             controller.Controller.move(controller.Controller.LEFT)
         }
         binding.btnRight.setOnClickListener {
+            vibrate()
             controller.Controller.move(controller.Controller.RIGHT)
         }
     }
